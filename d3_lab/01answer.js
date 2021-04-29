01696da9ef62ac57d20402c117f8a309c50743cc
geeman1209 <gabreu554@gmail.com>
2021-04-29 15:23:28 -0400

    adding d3_lab for deployment

---
+++ d3_lab/01answer.js
@@ -0,0 +1,29 @@
+d3.csv('ue_industry.csv', data => {
+
+    // Define your scales and generator here.
+      d3.csv('ue_industry.csv', data => {
+
+            console.log(data);
+        
+            const xScale = d3.scaleLinear()
+                .domain(d3.extent(data, d => +d.index))
+                .range([20, 1180]);
+            
+            const yScale = d3.scaleLinear()
+                .domain(d3.extent(data, d => +d.Agriculture))
+                .range([580, 20]);
+
+            let answerline = d3.line()
+            	.x(d => xScale(+d.index))
+            	.y(d => yScale(+d.Agriculture))
+        
+			d3.select('#answer1')
+			    .append('path')
+			    .attr('d', line(data))
+			    .attr('stroke', '#474af')
+        });
+
+    d3.select('#answer1')
+        // append more elements here
+
+});
